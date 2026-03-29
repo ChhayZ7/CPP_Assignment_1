70820347b568ff9c046aceb325e4411f1d62955f
ChhayZ7 <150762525+ChhayZ7@users.noreply.github.com>
2026-03-29 15:15:11 +1030

    Add Player.h with Player class declaration

--- Player.h
+++ Player.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <string>
+#include "Card.h"
+
+class Game;
+
+class Player {
+public:
+	Player();
+	~Player();
+
+	bool playCard(Card* card, Game& game);
+	void bankCard(Game& game);
+	void bustDiscard(Game& game);
+	bool isBust() const;
+	int getScore() const;
+	void printPlayerArea() const;
+	void printBank() const;
+
+	// Getters
+	const std::string& getName() const { return _name; }
+	CardCollection& getPlayArea() { return _playArea; }
+	CardCollection& getBank() { return _bank; }
+
+private:
+	std::string _name;
+	CardCollection _playArea;
+	CardCollection _bank;
+	void printCardCollection(const CardCollection& collection) const;
+
+};
