3f1a5e2287390843e27e57091155c706a93b19ff
ChhayZ7 <150762525+ChhayZ7@users.noreply.github.com>
2026-03-31 22:36:34 +1030

    Add OracleCard header

---
+++ OracleCard.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Card.h"
+#include <string>
+
+// OracleCard
+// Ability: peek at the top card of the deck before deciding whether to draw
+class OracleCard : public Card {
+public:
+	OracleCard(int value) : Card(value, CardType::Oracle) {}
+	std::string str() const override;
+	void play(Game& game, Player& player) override;
+};
