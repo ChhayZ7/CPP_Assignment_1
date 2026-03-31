a86fbc0714aba6eda70c54d027f46b2cfb954fb1
ChhayZ7 <150762525+ChhayZ7@users.noreply.github.com>
2026-03-31 19:42:01 +1030

    Add Game class interface and members

--- Game.h
+++ Game.h
@@ -5,6 +5,33 @@ class Player;
 
 class Game {
 public:
+	Game();
+		~Game();
+
+		void start();
+		void runTurn();
+		void endTurn();
+
+		Card* drawFromDeck();
+		Card* drawFromDiscard();
+		void addToDiscard(Card* card);
+		void shuffleDeck(CardCollection& cards);
+
+		Player* getCurrentPlayer() const;
+		Player* getOtherPlayer() const;
+		CardCollection& getDeck() { return _deck; }
+		CardCollection& getDiscard() { return _discard; }
+		int getTurn() const { return _turn; }
+		int getRound() const { return _round; }
 
 private:
+	CardCollection _deck;
+	CardCollection _discard;
+	Player* _players[2];
+	int _currentPlayer;
+	int _turn;
+	int _round;
+
+	void createDeck();
+	void printHeader() const;
 };
